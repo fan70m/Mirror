ad96bfdc2905fa2c2d5d0d59e5a282e198a5e762
Fany Chen <fanychen@live.ca>
2018-10-20 14:40:23 -0400

    Updates

---
+++ precache-manifest.d75b8e6791eaa7cd68e8fd9dd98d8c25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a56a946267b1495753d",
+    "url": "/mirror/static/js/runtime~main.6a56a946.js"
+  },
+  {
+    "revision": "1db6998d85dbb2e16f79",
+    "url": "/mirror/static/js/main.1db6998d.chunk.js"
+  },
+  {
+    "revision": "16739d81118632826863",
+    "url": "/mirror/static/js/1.16739d81.chunk.js"
+  },
+  {
+    "revision": "1db6998d85dbb2e16f79",
+    "url": "/mirror/static/css/main.85e95d98.chunk.css"
+  },
+  {
+    "revision": "237eeba1070cf44da8ce5adfe9a4cecb",
+    "url": "/mirror/index.html"
+  }
+];
